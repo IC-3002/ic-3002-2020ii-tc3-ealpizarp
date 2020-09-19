5dda3084b548178ed88634be2ebbf87906ac4a76
Eric Alpizar <ericalpizar@icloud.com>
2020-09-19 13:00:57 -0600

    Added cubic and constant behavioral for series calculations

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,22 @@
+
 def sumatoria_cubica(n):
-    raise NotImplementedError()
+    res = 0
+    i = 1
+    while i <= n:
+        j = 1
+        while j <= i:
+            k = j
+            while k <= (i + j):
+                res += 1
+                k += 1
+            j += 1
+        i +=1
+
+    return res
+
 
 def sumatoria_constante(n):
-    raise NotImplementedError()
 
+    res = (n ** 3 + 3 * 100 ** 2 + 2 * n) / 3
+
+    return res
